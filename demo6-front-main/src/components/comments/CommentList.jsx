97af991516882f9673cc5aa3fdb9393d81318dd1
Hana Im <qha_nap@nate.com>
2025-06-13 02:38:07 +0900

    250611 무한루프수정

--- demo6-front-main/src/components/comments/CommentList.jsx
+++ demo6-front-main/src/components/comments/CommentList.jsx
@@ -4,33 +4,33 @@ import useComment from "../../hooks/useComment";
 import useAuthStore from "../../stores/useAuthStore";
 
 const CommentList = ({comments}) => {
-    const username = useAuthStore(state => state.username);
-    const {onDelete} = useComment();
+  const username = useAuthStore(state=>state.username);
+  const {onDelete} = useComment();
 
     return (
-        <div>
-            {
-                comments.map(c => {
-                    return(
-                        <div key={c.cno}>
-                            <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                                <div>
-                                    <strong>{c.writer}</strong>&nbsp;&nbsp;
-                                    {
-                                        (c.writer === username) && <button className="btn btn-danger" onClick={()=>onDelete(c.cno, c.pno)}>삭제</button>
-                                    }
-                                </div>
-                            </div>
-                            <div>{c.writeTime}</div>
-                            <div>
-                                {c.content}
-                            </div>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    );
+    <div>
+      {
+        comments.map(c=> {
+          return (
+            <div>
+              <div style={{display:'flex', justifyContent:'space-between'}}>
+                <div>
+                  <strong>{c.writer}</strong>&nbsp; &nbsp;
+                  {
+                    (c.writer===username) && <button className="btn btn-danger" onClick={()=>onDelete(c.cno, c.pno)}>삭제</button>
+                  }
+                </div>
+              </div>
+              <div>{c.writeTime}</div>
+              <div>
+                {c.content}
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 };
 
 export default CommentList;
--- demo6-front-main/src/components/comments/CommentWrite.jsx
+++ demo6-front-main/src/components/comments/CommentWrite.jsx
@@ -6,12 +6,12 @@ const CommentWrite = ({pno}) => {
     const vc = useComment();
 
     return (
-        <div>
-            <label htmlFor='content' className="form-label">댓글 작성</label>
-            <textarea className="form-control" placeholder={vc.message} onChange={vc.onChange} value={vc.value} onBlur={vc.onBlur}></textarea>
-            <BlockButton styleName='primary' onClick={vc.onWrite(pno)} label='작성하기' />
-        </div>
-    );
+    <div>
+      <label htmlFor='content' className='form-label'>댓글 작성:</label>
+      <textarea className='form-control' placeholder={vc.message} onChange={vc.onChange} value={vc.value} onBlur={vc.onBlur}></textarea>
+      <BlockButton styleName='primary' onClick={()=>vc.onWrite(pno)} label="작성하기" />
+    </div>
+  )
 };
 
 export default CommentWrite;
